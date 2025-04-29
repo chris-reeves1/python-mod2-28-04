57632bdf7ed369af40ab57808a9418b38792d4f0
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-04-29 15:02:41 +0000

    2

--- collections.py
+++ collections.py
@@ -121,3 +121,99 @@ drinks = {"fizzy": "sprite", "hot": "coffee", "still": "water", "alcoholic": "wi
 # Have an input for author name
 # return a STRING of the books. ie not a list
 # error handling for incorrect keys (get)
+
+# book_dictionary = {
+# 	"N.T. Wright": ["The Bible for Everyone: A New Translation", "Surprised by Hope: Rethinking heaven, the resurrection and the mission of the Church"],
+# 	"Martin Luther": ["95 Theses", "The Bondage of the Will", "Table Talk"],
+# 	"Karl Barth": ["Church Dogmatics", "The Epistle to the Romans"]
+# }
+
+# author = input("Enter the name of the author: ")
+
+# if author in book_dictionary:
+# 	print("Books by " + author + ": ")
+# 	books = ", ".join(book_dictionary[author])
+# 	print(books)
+# else:
+# 	print("Cannot find any book by the author.")
+
+# booklist = {
+
+#     "J.K. Rowling": [
+#         "Harry Potter and the Sorcerer's Stone",
+#         "Harry Potter and the Chamber of Secrets",
+#         "Harry Potter and the Prisoner of Azkaban"
+#     ],
+#     "Roald Dahl": [
+#         "Charlie and the Chocolate Factory",
+#         "Matilda",
+#         "James and the Giant Peach"
+#     ],
+#     "Sheila McCullagh": [
+#         "The Land of the Blue Flower",
+#         "Tim and the Hidden People",
+#         "The Village with Three Corners"
+#     ]
+# }
+
+# print("Availible authors:")
+# print(", ".join(booklist.keys()))
+
+# author = input("please enter an authors name " )
+
+# books = booklist.get(author) or ["sorry no books found"]
+
+# print(", ".join(books))
+
+
+# JacksBookshop = {
+#   "author1": {
+#   "name": "Rick Riordan",
+#   "book1": "The Lightning Thief", 
+#   "book2": "The Sea of Monsters", 
+#   "book3": "The Last Olympian",
+# },
+#   "author2": {
+#   "name": "Jonathan Haidt",
+#   "book1": "The Happiness Hypothesis", 
+#   "book2": "The Righteous Mind",
+#   "book3": "The Anxious Generation",  
+# },
+#   "author3": {
+#   "name": "Colleen Hoover", 
+#   "book1": "It Ends With Us",
+#   "book2": "Verity",
+#   "book3": "Hopeless",    
+# }
+# }
+# y = JacksBookshop.get("author2").get("name")
+# print(y)
+
+# record  = JacksBookshop.get("author2")
+
+# list_to_print = []
+
+# for a, b in record.items():
+#     if "book" in a:
+#         list_to_print.append(b)
+
+# print(", ".join(list_to_print))
+
+# tuples
+
+#rectangle = 10, 5
+
+#rectangle[0] = 20
+
+#print(rectangle)
+
+# sets
+
+#set1 = {1, 2, 3, 4, 5}
+#set2= {4, 5, 6, 7, 8}
+
+#print(set1.union(set2))
+#print(set1.intersection(set2))
+
+#print(set1.difference(set2))
+#print(set1.symmetric_difference(set2))
---
+++ conditionals.py
@@ -0,0 +1,77 @@
+# if, elif, else
+# allows different pathways for our code to take, on certain conditions being met.
+
+#is_verified = False
+#is_admin = True
+#'on_holiday = False
+
+#if (is_admin or is_verified) and not on_holiday:
+#    print("access granted")
+#else:
+#    print("no access")
+
+#import dis
+
+#def example():
+#    x = 0
+#    while x < 3:
+#        x += 1
+
+#dis.dis(example)
+
+# if elif else
+
+#temp = 23
+
+#if temp >= 30:
+#    print("its very hot")
+#elif temp > 25:
+#    print("still quite warm")
+#elif temp > 10:
+#    print("it ok")
+#else:
+#    print("not good")
+
+
+# multiple comparators
+
+#deposit = 100
+#password = "password2"
+
+#if 0 <  deposit < 100 and password == "password":
+#    print("deposit ok")
+#else:
+#    print("no allowed")
+
+# not
+
+#if not 0 < deposit < 100 and password != "password":
+#    print("not allowed")
+#else:
+#    print("thanks")
+
+
+# in and not in
+
+#name = "root"
+
+#if name in ("root", "admin", "password"):
+#    print("invalid")
+#else:
+#    print("accpeted")
+
+#if name not in ("root", "admin", "password"):
+#    print("accepted")
+#else:
+#    print("denied")
+
+# exercise:
+
+# weight converter app
+# user to input weight
+# user to input kgs or lbs
+# logic: check unit input
+# logic: convert to corresponding unit. (kgs - lbs)
+# print out value. 
+# error handling: unit input (loops/upper/lower)
+# optional: error handling for non-numeric input in weight.  
