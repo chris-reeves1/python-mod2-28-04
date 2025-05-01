81945fe99dbbbc9de1c2aa715de71c8a00145225
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-05-01 13:16:25 +0000

    2

---
+++ other/feedback_task.py
@@ -0,0 +1,32 @@
+- An app that will help automate the writing of student feedback adhering to 
+    formatting requirements. 
+
+    reqs:
+        - The feedback must be written to a .txt file with the file name 
+            including the students name. 
+        - The text in the file must have the headers 'General comments', 
+            'Punctuality and engagement' and 'further learning'. Each header will have
+            a short paragraph of text.
+        - All files to be written to a sub-folder named "feedback_files"
+        - The file should open automatically in a text editor for manual review.
+        - The user of the app should input students names.
+        - Inputs for marks/grades students recieve for specific factors such    
+            as 'understanding_level', 'contribution_level' etc. 
+        - The marks should be used to format believable sentances to be written 
+            to the file.
+        - The feedback is expected to have correct grammar and appear to be 
+            written personally.
+
+
+example final format:
+
+General comments
+Lyudmil worked hard in this module, completing all his labs to a good standard. 
+He picked up DevOps tooling quickly, demonstrating a good level of existing knowledge. 
+He contributed to class discussions and worked well independently.  
+
+Learner Punctuality and engagement 
+Lyudmil was always punctual throughout the module and engaged well. 
+
+Recommendations on further learning
+Continue to practice and explore good pipeline design and integrate docker and monitoring into your projects. 
