977fcb10aa7f4d160308e261f7efde566daa5476
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-05-07 13:01:50 +0000

    2

--- flask/password_checker.py
+++ flask/password_checker.py
@@ -1,13 +1,3 @@
-# reqs
-# need a class
-# init - password to be passed through on object creation. 
-# 1 method to return strength of self.password:
-# returns a string:
-# very weak - weak- medium - strong - very strong
-# length, !alphanumeric, capitals
-# check against a small list of known passwords. - return very weak by default.  
-# avoid dependancies (core logic)
-
 class PasswordChecker:
     def __init__(self, password):
         self.password = password
