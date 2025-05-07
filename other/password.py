cce47c097aa0f171a09a57c8b0011cb73009a374
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-05-07 08:51:41 +0000

    2

---
+++ other/password.py
@@ -0,0 +1,9 @@
+# reqs
+# need a class
+# init - password to be passed through on object creation. 
+# 1 method to return strength of self.password:
+# returns a string:
+# very weak - weak- medium - strong - very strong
+# length, !alphanumeric, capitals
+# check against a small list of known passwords. - return very weak by default.  
+# avoid dependancies (core logic)
